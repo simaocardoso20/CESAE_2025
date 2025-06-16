efc2fc8fa2f94e4fd0c4d15705e04549afb9daa7
simaocardoso20 <simaoprof@hotmail.com>
2025-06-16 16:38:34 +0100

    Enter

--- Indiana_Jones_A_Maldicao_Da_Serpente_Dourada/src/Indy/Jogo.java
+++ Indiana_Jones_A_Maldicao_Da_Serpente_Dourada/src/Indy/Jogo.java
@@ -140,24 +140,37 @@ public class Jogo {
         }
     }
 
+    private static void esperarEnter(Scanner scanner) {
+        System.out.print("\n(Pressiona Enter para continuar...\n)");
+        scanner.nextLine();
+    }
+
     public static void introducaoHistoria() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("\n=== INTRODUÇÃO ===\n");
-        System.out.println("Ano de 1938. A Europa estremece às portas de uma guerra iminente, mas longe das grandes potências,\n"
-                + "nos confins do Sudeste Asiático, uma ameaça mais antiga desperta.\n"
-                + "O mundo mergulha nas sombras à medida que forças obscuras procuram artefactos lendários.\n"
-                + "A Serpente Dourada, uma relíquia amaldiçoada com poderes místicos, reapareceu nos confins de um templo esquecido.\n"
-                + "Dizem as lendas locais que a estátua, feita de ouro puro, representa uma deusa-serpente capaz de libertar tempestades tropicais\n"
-                + "e despertar um exército adormecido de guerreiros espectrais.\n"
-                + "Diz-se que quem a possuir poderá dominar os reinos da vida e da morte...\n"
-                + "Cabe-te a ti, corajoso aventureiro, enfrentar este labirinto de perigos e escolher o teu destino.\n"
-                + "Boa sorte — o teu caminho começa agora!\n");
+        System.out.println("Ano de 1938. A Europa estremece às portas de uma guerra iminente, mas longe das grandes potências,");
+        System.out.println("nos confins do Sudeste Asiático, uma ameaça mais antiga desperta.");
+        System.out.println("O mundo mergulha nas sombras à medida que forças obscuras procuram artefactos lendários.");
+                esperarEnter(scanner);
+        System.out.println("A Serpente Dourada, uma relíquia amaldiçoada com poderes místicos, reapareceu nos confins de um templo esquecido.");
+        System.out.println("Dizem as lendas locais que a estátua, feita de ouro puro, representa uma deusa-serpente capaz de libertar tempestades tropicais");
+        System.out.println("e despertar um exército adormecido de guerreiros espectrais.");
+                esperarEnter(scanner);
+        System.out.println("Diz-se que quem a possuir poderá dominar os reinos da vida e da morte...");
+        System.out.println("Cabe-te a ti, corajoso aventureiro, enfrentar este labirinto de perigos e escolher o teu destino.");
+        System.out.println("Boa sorte — o teu caminho começa agora!\n");
+                esperarEnter(scanner);
     }
 
     public static void mostrarRoteiroPersonalizado(Heroi heroi) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("\n=== MISSÃO 1 - BEM-VINDO À SELVA ===\n");
-        System.out.println("No coração da selva peruana, um antigo artefacto – A Serpente Dourada de Uxmal –\n"
-                + "foi descoberto por exploradores ilegais.\n"
-                + "Diz a lenda que quem a possui controla as forças da natureza.\n");
+        System.out.println("No coração da selva peruana, um antigo artefacto – A Serpente Dourada de Uxmal –")
+        System.out.println("foi descoberto por exploradores ilegais.")
+        System.out.println("Diz a lenda que quem a possui controla as forças da natureza.\n");
+                esperarEnter(scanner);
 
         // Personalizar o texto consoante a escolha do herói
         String classeHeroi = heroi.getClass().getSimpleName();
@@ -165,28 +178,34 @@ public class Jogo {
 
         switch (classeHeroi) {
             case "IndianaJones":
-                partePersonalizada = "Indiana Jones fará tudo para recuperá-la antes que caia nas mãos erradas, impedindo assim uma catástrofe!!!\n"
-                        + "Depois de saber dos perigos que o aguardavam, Indy foi ao mercado procurar o seu amigo de longa data, Tupaq Callani.\n"
-                        + "Um velho guia nativo da selva peruana, astuto, leal e com um conhecimento lendário sobre ruínas esquecidas.\n"
-                        + "Velho amigo de Indiana Jones, já lhe salvou a vida mais vezes do que ele gosta de admitir.\n"
-                        + "Vê o que Tupaq Callani tem para te oferecer, sê sábio na tua escolha!";
+                partePersonalizada = "Indiana Jones fará tudo para recuperá-la antes que caia nas mãos erradas, impedindo assim uma catástrofe!!!");
+                System.out.println("Depois de saber dos perigos que o aguardavam, Indy foi ao mercado procurar o seu amigo de longa data, Tupaq Callani.");
+                System.out.println("Um velho guia nativo da selva peruana, astuto, leal e com um conhecimento lendário sobre ruínas esquecidas.");
+                        esperarEnter(scanner);
+                System.out.println("Velho amigo de Indiana Jones, já lhe salvou a vida mais vezes do que ele gosta de admitir.");
+                System.out.println("Vê o que Tupaq Callani tem para te oferecer, sê sábio na tua escolha!\n");
+                        esperarEnter(scanner);
                 break;
             case "HansVogel":
-                partePersonalizada = "Hans Vogel pretende usá-la como arma de dominação mundial,\n"
-                        + "moldando o destino da humanidade com punho de ferro!!!\n"
-                        + "Calculista, Hans sabia que tinha que estar preparado para enfrentar os perigos que o aguardavam.\n"
-                        + "Um velho guia nativo da selva peruana, com um conhecimento lendário sobre ruínas esquecidas,\n"
-                        + "encontra-se no mercado e tem tudo o que Hans precisa para atingir os seus objetivos.\n"
-                        + "Vê o que o velho peruano tem para te oferecer, sê sábio na tua escolha!";
+                partePersonalizada = "Hans Vogel pretende usá-la como arma de dominação mundial,");
+                System.out.println("moldando o destino da humanidade com punho de ferro!!!");
+                System.out.println("Calculista, Hans sabia que tinha que estar preparado para enfrentar os perigos que o aguardavam.");
+                System.out.println("Um velho guia nativo da selva peruana, com um conhecimento lendário sobre ruínas esquecidas,");
+                        esperarEnter(scanner);
+                System.out.println("encontra-se no mercado e tem tudo o que Hans precisa para atingir os seus objetivos.");
+                System.out.println("Vê o que o velho peruano tem para te oferecer, sê sábio na tua escolha!\n");
+                        esperarEnter(scanner);
                 break;
             case "SophieValen":
-                partePersonalizada = "Sophie Valen vê neste artefacto uma peça vital do seu passado sombrio,\n"
-                        + "mas também uma ameaça que poderá destruir tudo!!!\n"
-                        + "Sedutora e esperta, Sophie conseguiu convencer o proprietário do hotel onde estava hospedada,\n"
-                        + "a fornecer-lhe um conjunto de items raros e proibidos.\n"
-                        + "Entre olhares furtivos e palavras sussurradas ao pé do ouvido,\n"
-                        + "ela obteve acesso ao velho arsenal escondido no porão do edifício.\n"
-                        + "Vê o que o dono do hotel tem para te oferecer, sê sábia na tua escolha!";
+                partePersonalizada = "Sophie Valen vê neste artefacto uma peça vital do seu passado sombrio,");
+                System.out.println("mas também uma ameaça que poderá destruir tudo!!!");
+                System.out.println("Sedutora e esperta, Sophie conseguiu convencer o proprietário do hotel onde estava hospedada,");
+                System.out.println("a fornecer-lhe um conjunto de items raros e proibidos.");
+                        esperarEnter(scanner);
+                System.out.println("Entre olhares furtivos e palavras sussurradas ao pé do ouvido,");
+                System.out.println("ela obteve acesso ao velho arsenal escondido no porão do edifício.");
+                System.out.println("Vê o que o dono do hotel tem para te oferecer, sê sábia na tua escolha!\n");
+                        esperarEnter(scanner);
                 break;
             default:
                 partePersonalizada = "Este aventureiro desconhecido enfrenta a selva sem saber que o seu destino será alterado para sempre.";
@@ -238,12 +257,15 @@ public class Jogo {
     }
 
     public static void missaoSelva(Heroi heroi) {
-        System.out.println("Com os bolsos mais leves (ou não), entras na espessa e abafada selva.\n"
-                + "O som distante de tambores tribais mistura-se com os gritos de animais selvagens.\n"
-                + "A missão é clara: encontrar o mapa perdido escondido dentro desta floresta traiçoeira.\n"
-                + "Dois caminhos surgem à tua frente: ");
-
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Com os bolsos mais leves (ou não), entras na espessa e abafada selva.");
+        System.out.println("O som distante de tambores tribais mistura-se com os gritos de animais selvagens.");
+        esperarEnter(scanner);
+        System.out.println("A missão é clara: encontrar o mapa perdido escondido dentro desta floresta traiçoeira.");
+        System.out.println("Dois caminhos surgem à tua frente: \n");
+
+
         System.out.println("1. ➡️ O Trilho dos Macacos: Não estás sozinho, os sons de algo (ou alguém) a mover-se entre os galhos são constantes.");
         System.out.println("2. ➡️ A Terra dos Mortos: Um atalho sinistro, com solo instável, lama densa e um perigo silencioso.\n");
 
@@ -254,7 +276,8 @@ public class Jogo {
             case 1:
                 System.out.println("\nEscolheste o Trilho dos Macacos e avanças cautelosamente...");
                 System.out.println("De repente, surge um macaco, parece ser o líder do bando e bloqueia o teu caminho!");
-                System.out.println("Boa sorte, estás por tua conta!");
+                esperarEnter(scanner);
+                System.out.println("Boa sorte, estás por tua conta!\n");
 
                 // Luta com o macaco lider
                 boolean venceu = heroi.atacar(Jogo.macacoLider);
@@ -266,16 +289,18 @@ public class Jogo {
                     System.out.println("Como recompensa e demonstração de respeito, o macaco deu-te o mapa que procuravas!");
                 } else {
                     System.out.println("Foste derrotado! Não tiveste qualquer hipótese. É melhor não tentares novamente!");
-                    System.out.println("Ainda te vais magoar!");
+                    System.out.println("Ainda te vais magoar!\n");
                 }
                 break;
 
             case 2:
                 System.out.println("\nAvanças pela passagem enlameada... acho que não foi boa ideia.");
                 System.out.println("O solo cede de repente! Areia movediiiiiiiiiça!");
+                esperarEnter(scanner);
                 heroi.perderVida(40);
                 System.out.println("Com muito esforço consegues escapar, quase que morrias, mas mesmo assim perdes 40 pontos de vida.");
-                System.out.println("No final do trilho, encontras umas pegadas e decides segui-las.\n");
+                System.out.println("No final do trilho, encontras pegadas e decides segui-las.\n");
+                esperarEnter(scanner);
                 break;
 
             default:
@@ -288,9 +313,11 @@ public class Jogo {
 
         System.out.println("\n=== MISSÃO 2 – O TEMPLO DOS ECOS PERDIDOS ===\n");
         System.out.println("Com o mapa em mãos, atravessas a última barreira de vegetação densa...");
-        System.out.println("Diante de ti ergue-se um templo milenar coberto por raízes e musgo.\n");
-        System.out.println("No seu interior, ecoam sons metálicos... algo se move.\n");
-        System.out.println("Dois caminhos apresentam-se logo à entrada:");
+        System.out.println("Diante de ti ergue-se um templo milenar coberto por raízes e musgo.");
+        esperarEnter(scanner);
+        System.out.println("No seu interior, ecoam sons metálicos... algo se move.");
+        System.out.println("Dois caminhos apresentam-se logo à entrada:\n");
+        esperarEnter(scanner);
 
         System.out.println("1. ➡️ Porta de Pedra – Uma entrada imponente com inscrições antigas. Um cheiro a óleo queimado no ar.");
         System.out.println("2. ➡️ Túnel Subterrâneo – Escuro, húmido, onde só os morcegos ousam entrar.\n");
@@ -302,17 +329,21 @@ public class Jogo {
             case 1:
                 System.out.println("\nAvanças pela Porta de Pedra...cuidado onde pões os pés!");
                 System.out.println("Pisos móveis, sensores escondidos e uma armadilha dispara!");
+                esperarEnter(scanner);
                 System.out.println("➡️ Lançamento automático de flechas!");
                 heroi.perderVida(30);
-                System.out.println("Foste atingido por uma flecha! Perdes 30 pontos de vida.");
+                System.out.println("Foste atingido por uma flecha! Perdes 30 pontos de vida.\n");
+                esperarEnter(scanner);
                 break;
 
             case 2:
                 System.out.println("\nAvanças pelo túnel subterrâneo com cuidado...");
                 System.out.println("De repente – *click*! Uma lança surge do chão!");
+                esperarEnter(scanner);
                 System.out.println("➡️ Tentas desviar-te mas foste ferido.");
                 heroi.perderVida(20);
-                System.out.println("Levas um corte profundo e perdes 20 pontos de vida.");
+                System.out.println("Levas um corte profundo e perdes 20 pontos de vida.\n");
+                esperarEnter(scanner);
                 break;
 
             default:
@@ -323,19 +354,22 @@ public class Jogo {
 
     public static void desafioChaveCristal(Heroi heroi) {
         Scanner scanner = new Scanner(System.in);
+
         String respostaCorreta = "livro";
         int tentativas = 0;
         boolean acertou = false;
 
         System.out.println("\nApesar dos ferimentos, avanças para o centro do templo.");
         System.out.println("Sobre um pedestal de pedra, envolta em luz azulada, repousa uma pequena — mas poderosa — **Chave de Cristal**!");
+        esperarEnter(scanner);
         System.out.println("Ao esticares a mão para a agarrar, ouves uma voz antiga ecoar pelas paredes de pedra:");
         System.out.println("\"Responde se fores digno. Uma única palavra é a chave. Se falhares... o templo decidirá teu destino.\"\n");
+        esperarEnter(scanner);
 
-        System.out.println("🔐 Adivinha: \"Vivo entre páginas, mas não sou animal.\n"
-                + "Revelo o antigo, o sagrado e o mortal.\n"
-                + "Quem me decifra, ganha poder imortal.\n"
-                + "O que é?");
+        System.out.println("🔐 Adivinha: \"Vivo entre páginas, mas não sou animal.");
+        System.out.println("Revelo o antigo, o sagrado e o mortal.");
+        System.out.println("Quem me decifra, ganha poder imortal.");
+        System.out.println("O que é?");
 
         while (tentativas < 3 && !acertou) {
             System.out.print("A tua resposta: ");
@@ -346,6 +380,7 @@ public class Jogo {
                 System.out.println("\n✨ A luz azulada envolve-te — acertaste!");
                 System.out.println("O templo reconhece a tua sabedoria... e concede-te acesso ao seu maior segredo.");
                 System.out.println("A Chave de Cristal flutua até à tua mão. Foste digno!");
+                esperarEnter(scanner);
                 System.out.println("A tua vida é restaurada e ganhas 10 pontos de força como recompensa pela tua inteligência.");
                 heroi.recuperarVida(heroi.getVidaMax()); // restaura a vida
                 heroi.ganharForca(10);
