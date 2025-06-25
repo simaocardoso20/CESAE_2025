c1009f09e22c8815840a123ed4a88ff7315b4a9a
alexandramst <alexandratorres14@gmail.com>
2025-06-25 12:39:04 +0100

    Hello

--- src/Repositories/AtracaosRepo.java
+++ src/Repositories/AtracaosRepo.java
@@ -6,8 +6,5 @@ import java.util.ArrayList;
 
 public class AtracaosRepo {
 
-    private ArrayList<Atracao> atracaoArray;
 
-    public AtracaosRepo() throws  {
-    }
 }
--- src/Tools/CSVFileReader.java
+++ src/Tools/CSVFileReader.java
@@ -1,7 +1,9 @@
 package Tools;
 
 import Models.Atracao;
+import Models.Custo;
 import Models.User;
+import Models.Venda;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,7 +12,7 @@ import java.util.Scanner;
 
 public class CSVFileReader {
 
-    public ArrayList<Atracao> salesFileReader(String filePath) throws FileNotFoundException {
+    public ArrayList<Atracao> atracaosFileReader(String filePath) throws FileNotFoundException {
 
         // Abrimos o ficheiro e criamos um Scanner associado
         File salesFile = new File(filePath);
@@ -50,48 +52,43 @@ public class CSVFileReader {
         return atracaoArray;
     }
 
-    public ArrayList<Atracao> salesFileReader(String filePath) throws FileNotFoundException {
+    public ArrayList<Custo> custosFileReader(String filePath) throws FileNotFoundException {
 
         // Abrimos o ficheiro e criamos um Scanner associado
-        File salesFile = new File(filePath);
-        Scanner salesScanner = new Scanner(salesFile);
+        File custosFile = new File(filePath);
+        Scanner custosScanner = new Scanner(custosFile);
 
         // Criamos o ArrayList de Vendas, vazio
-        ArrayList<Atracao> atracaoArray = new ArrayList<>();
+        ArrayList<Custo> custosArray = new ArrayList<>();
 
         // Avançar o cabeçalho
-        salesScanner.nextLine();
+        custosScanner.nextLine();
 
         // Ciclo que vai correr para cada linha do ficheiro
-        while (salesScanner.hasNextLine()) {
+        while (custosScanner.hasNextLine()) {
 
             // Apanhamos a linha
-            String linha = salesScanner.nextLine();
+            String linha = custosScanner.nextLine();
 
             // Separamos a linha, com base no seu separador ","
             String[] linhaSeparada = linha.split(";");
 
             // Reunir todas as informações da Venda
             int idAtracao =  Integer.parseInt(linhaSeparada[0]);
-            String atracao = linhaSeparada[1];
-            double precoAdulto = Double.parseDouble(linhaSeparada[2]);
-            double precoCrianca = Double.parseDouble(linhaSeparada[3]);
-            int duracaoSeg =  Integer.parseInt(linhaSeparada[4]);
+            double custoManutencao = Double.parseDouble(linhaSeparada[1]);
+            double custoFixo = Double.parseDouble(linhaSeparada[2]);
 
             // Criar nova Venda
-            Atracao newAtracao = new Atracao(idAtracao,atracao,precoAdulto,precoCrianca, duracaoSeg);
+            Custo newCusto = new Custo(idAtracao, custoManutencao, custoFixo);
 
             // Adicionamos ao Array
-            atracaoArray.add(newAtracao);
+            custosArray.add(newCusto);
 
         }
 
         // Terminando o ciclo (não há mais linhas)
-        return atracaoArray;
+        return custosArray;
     }
-
-
-
 
 
     public ArrayList<User> usersFileReader(String filePath) throws FileNotFoundException {
@@ -130,5 +127,49 @@ public class CSVFileReader {
 
         // Terminando o ciclo (não há mais linhas)
         return usersArray;
+    }
+
+
+    public ArrayList<Venda> vendasFileReader(String filePath) throws FileNotFoundException {
+
+        // Abrimos o ficheiro e criamos um Scanner associado
+        File vendasFile = new File(filePath);
+        Scanner vendasScanner = new Scanner(vendasFile);
+
+        // Criamos o ArrayList de Vendas, vazio
+        ArrayList<Venda> vendasArray = new ArrayList<>();
+
+        // Avançar o cabeçalho
+        vendasScanner.nextLine();
+
+        // Ciclo que vai correr para cada linha do ficheiro
+        while (vendasScanner.hasNextLine()) {
+
+            // Apanhamos a linha
+            String linha = vendasScanner.nextLine();
+
+            // Separamos a linha, com base no seu separador ","
+            String[] linhaSeparada = linha.split(";");
+
+            // Reunir todas as informações da Venda
+            int idAtracao =  Integer.parseInt(linhaSeparada[0]);
+
+            String data = linhaSeparada[1];
+            String[] dataSeparada = data.split("/");
+            int mes = Integer.parseInt(dataSeparada[0]);
+            int ano = Integer.parseInt(dataSeparada[1]);
+
+            String tipoCliente = linhaSeparada[2];
+
+            // Criar nova Venda
+            Venda newVenda = new Venda(idAtracao, mes, ano, tipoCliente);
+
+            // Adicionamos ao Array
+            vendasArray.add(newVenda);
+
+        }
+
+        // Terminando o ciclo (não há mais linhas)
+        return vendasArray;
     }
 }
