68a9346eefe0a06fa9a9a03cb33856f4deeaa8ca
simaocardoso20 <simaoprof@hotmail.com>
2025-07-05 21:44:37 +0100

    Renomeada a pasta 'CESAE Resort' para 'CESAE_Resort'

--- CESAE_Resort/src/Main.java
+++ CESAE_Resort/src/Main.java
@@ -3,8 +3,10 @@ import Views.LoginView;
 import java.io.FileNotFoundException;
 
 public class Main {
-
     public static void main(String[] args) throws FileNotFoundException {
+
+        // Mostra o diretório de trabalho atual
+        System.out.println("Working dir: " + System.getProperty("user.dir"));
 
         LoginView lv = new LoginView();
         lv.mainMenu();
