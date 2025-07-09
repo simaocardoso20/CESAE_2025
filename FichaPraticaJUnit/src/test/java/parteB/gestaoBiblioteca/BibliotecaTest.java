ec88f850aba50e152c9e9ff4c423c785f77dc8ed
simaocardoso20 <simaoprof@hotmail.com>
2025-07-09 09:23:54 +0100

    test biblioteca

--- FichaPraticaJUnit/src/test/java/parteB/gestaoBiblioteca/BibliotecaTest.java
+++ FichaPraticaJUnit/src/test/java/parteB/gestaoBiblioteca/BibliotecaTest.java
@@ -1,12 +1,54 @@
 package parteB.gestaoBiblioteca;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BibliotecaTest {
 
+    Livro headsFirstJava;
+    Livro hobbit;
+    Biblioteca aMinhaBiblioteca;
+
     @BeforeEach
     void setUp() {
+        this.headsFirstJava = new Livro("Heads First Java", "Kathy Sierra", 2015, 760);
+        this.hobbit = new Livro("Hobbit", "Tolkien", 2000, 600);
+
+        this.aMinhaBiblioteca = new Biblioteca();
     }
+
+    @Test
+    public void testAdicionarLivro() {
+        assertEquals(0, this.aMinhaBiblioteca.contarLivros());
+
+        this.aMinhaBiblioteca.adicionarLivro(headsFirstJava);
+
+        assertEquals(1, this.aMinhaBiblioteca.contarLivros());
+
+        this.aMinhaBiblioteca.adicionarLivro(hobbit);
+
+        assertEquals(2, this.aMinhaBiblioteca.contarLivros());
+
+        assertEquals("Título: Heads First Java, Autor: Kathy Sierra, Ano: 2015, Páginas: 760, Estado: disponível\nTítulo: Hobbit, Autor: Tolkien, Ano: 2000, Páginas: 600, Estado: disponível", this.aMinhaBiblioteca.listarLivros());
+        assertEquals(this.headsFirstJava.obterResumo() + "\n" + this.hobbit.obterResumo(), this.aMinhaBiblioteca.listarLivros());
+
+    }
+
+    @Test
+    public void testRemoverLivroPorTitulo() {
+        this.aMinhaBiblioteca.adicionarLivro(headsFirstJava);
+        this.aMinhaBiblioteca.adicionarLivro(hobbit);
+
+        this.aMinhaBiblioteca.removerLivroPorTitulo("Heads First Java");
+
+        assertEquals(1, this.aMinhaBiblioteca.contarLivros());
+
+        assertEquals(this.hobbit.obterResumo(), this.aMinhaBiblioteca.listarLivros());
+
+    }
+
+
 }
--- FichaPraticaJUnit/src/test/java/parteB/gestaoBiblioteca/LivroTest.java
+++ FichaPraticaJUnit/src/test/java/parteB/gestaoBiblioteca/LivroTest.java
@@ -1,12 +1,67 @@
 package parteB.gestaoBiblioteca;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LivroTest {
 
+    Livro headsFirstJava;
+    Livro hobbit;
+
     @BeforeEach
     void setUp() {
+        this.headsFirstJava = new Livro("Heads First Java", "Kathy Sierra", 2015, 760);
+        this.hobbit = new Livro("Hobbit", "Tolkien", 2000, 600);
+    }
+
+    @Test
+    public void testGetTitulo() {
+        assertEquals("Heads First Java", this.headsFirstJava.getTitulo());
+        assertEquals("Hobbit", this.hobbit.getTitulo());
+    }
+
+    @Test
+    public void testAlterarTitulo() {
+        assertEquals("Heads First Java", this.headsFirstJava.getTitulo());
+        this.headsFirstJava.alterarTitulo("Heads First Python");
+        assertEquals("Heads First Python", this.headsFirstJava.getTitulo());
+        this.headsFirstJava.alterarTitulo("Heads First C++");
+        assertEquals("Heads First C++", this.headsFirstJava.getTitulo());
+
+    }
+
+    @Test
+    public void testEmprestarLivroDisponivel() {
+        assertTrue(this.headsFirstJava.estaDisponivel());
+        this.headsFirstJava.emprestar();
+        assertFalse(this.headsFirstJava.estaDisponivel());
+
+        // CONTROLO
+        assertTrue(this.hobbit.estaDisponivel());
+        this.hobbit.emprestar();
+        assertFalse(this.hobbit.estaDisponivel());
+    }
+
+    @Test
+    public void testEmprestarLivroIndisponivel(){
+        this.headsFirstJava.emprestar();
+        assertFalse(this.headsFirstJava.estaDisponivel());
+        this.headsFirstJava.emprestar();
+        assertFalse(this.headsFirstJava.estaDisponivel());
+
+        // CONTROLO
+        this.hobbit.emprestar();
+        assertFalse(this.hobbit.estaDisponivel());
+        this.hobbit.emprestar();
+        assertFalse(this.hobbit.estaDisponivel());
+    }
+
+    @Test
+    public void testRecepcionarLivroEmprestado(){
+        this.headsFirstJava.emprestar();
+        this.headsFirstJava.recepcionar();
+        assertTrue(this.headsFirstJava.estaDisponivel());
     }
 }
